119ebdff6afcf64d4c5280d48a8a2f1400ce1837
Nathnael Desta <destanathnaelataro@gmail.com>
2025-03-04 16:00:51 +0300

    feat: add basic login form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -14,6 +14,7 @@ import { authClient } from "@/lib/auth-client";
 import { signInFormSchema } from "@/lib/auth-schema";
 import Image from "next/image";
 import Login from "@/public/images/log-in.png"
+import { Checkbox } from "@/components/ui/checkbox";
 
 const BaseUrl = process.env.API_URL
 
@@ -70,59 +71,63 @@ export default function SignIn() {
       <div className="h-[calc(100vh/2)] w-full">
         <Image src={Login} alt="login image" className=" w-full h-full object-cover" />
       </div>
-      <div className="flex-1 border-4 border-red-400"></div>
+      <Card className="flex-1 w-full px-8 flex flex-col justify-center">
+        <CardHeader>
+          <CardTitle className="font-bold text-2xl text-[#060A87] text-center">Login to your account</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-s font-medium text-[#111827]">Email <span className="text-[#E03137]">*</span></FormLabel>
+                    <FormControl>
+                      <Input placeholder="john@mail.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-s font-medium text-[#111827]">Password <span className="text-[#E03137]">*</span></FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="Enter your password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between ml-[-5px] mr-[-5px] items-center">
+                <div className="flex gap-1.5 items-center">
+                  <Checkbox className="data-[state=checked]:bg-[#27A376] data-[state=checked]:border-[#27A376]" />
+                  <div className="text-[#687588] font-medium text-s mb-[-2px]">Remember Me</div>
+                </div>
+                <div className="text-[#687588] font-medium text-s items-center cursor-default">Forgot Password</div>
+              </div>
+              <Button className="w-full" type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+
+        <CardFooter className='flex justify-center'>
+          <p className='text-sm text-muted-foreground'>
+            Don&apos;t have an account yet?{' '}
+            <Link href='/sign-up' className='text-primary hover:underline'>
+              Sign up
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+
     </div>
-    // <Card className="w-full max-w-md mx-auto">
-    //   <CardHeader>
-    //     <CardTitle>Sign In</CardTitle>
-    //     <CardDescription>
-    //       Welcome back! Please sign in to continue.
-    //     </CardDescription>
-    //   </CardHeader>
-
-    //   <CardContent>
-    //     <Form {...form}>
-    //       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-    //         <FormField
-    //           control={form.control}
-    //           name="email"
-    //           render={({ field }) => (
-    //             <FormItem>
-    //               <FormLabel>Email</FormLabel>
-    //               <FormControl>
-    //                 <Input placeholder="john@mail.com" {...field} />
-    //               </FormControl>
-    //               <FormMessage />
-    //             </FormItem>
-    //           )}
-    //         />
-    //         <FormField
-    //           control={form.control}
-    //           name="password"
-    //           render={({ field }) => (
-    //             <FormItem>
-    //               <FormLabel>Password</FormLabel>
-    //               <FormControl>
-    //                 <Input type="password" placeholder="Enter your password" {...field} />
-    //               </FormControl>
-    //               <FormMessage />
-    //             </FormItem>
-    //           )}
-    //         />
-    //         <Button className="w-full" type="submit">Submit</Button>
-    //       </form>
-    //     </Form>
-    //   </CardContent>
-
-    //   <CardFooter className='flex justify-center'>
-    //     <p className='text-sm text-muted-foreground'>
-    //       Don&apos;t have an account yet?{' '}
-    //       <Link href='/sign-up' className='text-primary hover:underline'>
-    //         Sign up
-    //       </Link>
-    //     </p>
-    //   </CardFooter>
-    // </Card>
 
   )
 }
