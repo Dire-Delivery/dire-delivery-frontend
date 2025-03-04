e1bcf14402a296219806d9a94550350cae9bafae
Nathnael Desta <destanathnaelataro@gmail.com>
2025-03-04 16:56:34 +0300

    feat: finish design of desktop side

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -68,26 +68,26 @@ export default function SignIn() {
   }
 
   return (
-    <div className="flex flex-col justify-between h-screen w-screen">
-      <div className="h-[calc(100vh/2.5)] w-full">
+    <div className="flex flex-col justify-between h-screen w-screen md:flex-row">
+      <div className="h-[calc(100vh/2.5)] w-full md:h-screen md:w-[calc(100vw/2)]">
         <Image src={Login} alt="login image" className=" w-full h-full object-cover" />
       </div>
-      <Card className="flex-1 w-full px-8 flex flex-col justify-center">
+      <Card className="flex-1 w-full px-8 flex flex-col justify-center md:flex-none md:w-[350px] md:p-0 md:mx-auto md:border-none md:shadow-none lg:w-[450px] xl:w-[500px] 2xl:w-[550px]">
         <CardHeader className="p-0">
-          <CardTitle className="font-bold text-2xl text-[#060A87] text-center p-0 px-0 pb-6">Login to your account</CardTitle>
+          <CardTitle className="font-bold text-2xl text-[#060A87] text-center p-0 px-0 pb-6 md:text-3xl">Login to your account</CardTitle>
         </CardHeader>
 
         <CardContent className="pb-0">
           <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 md:space-y-7">
               <FormField
                 control={form.control}
                 name="email"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="text-s font-medium text-[#111827]">Email <span className="text-[#E03137]">*</span></FormLabel>
+                    <FormLabel className="text-s font-medium text-[#111827] md:text-lg">Email <span className="text-[#E03137]">*</span></FormLabel>
                     <FormControl>
-                      <Input placeholder="john@mail.com" className="h-10" {...field} />
+                      <Input placeholder="john@mail.com" className="h-10 md:h-12 md:text-base" {...field} />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -98,9 +98,9 @@ export default function SignIn() {
                 name="password"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel className="text-s font-medium text-[#111827]">Password <span className="text-[#E03137]">*</span></FormLabel>
+                    <FormLabel className="text-s font-medium text-[#111827] md:text-lg">Password <span className="text-[#E03137]">*</span></FormLabel>
                     <FormControl>
-                      <PasswordInput placeholder="Enter your password" className="h-10" {...field}/>
+                      <PasswordInput placeholder="Enter your password" className="h-10 md:h-12 md:text-base" {...field}/>
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -109,9 +109,9 @@ export default function SignIn() {
               <div className="flex justify-between ml-[-5px] mr-[-5px] items-center">
                 <div className="flex gap-1.5 items-center">
                   <Checkbox className="data-[state=checked]:bg-[#27A376] data-[state=checked]:border-[#27A376]" />
-                  <div className="text-[#687588] font-medium text-s mb-[-3px]">Remember Me</div>
+                  <div className="text-[#687588] font-medium text-s mb-[-3px] cursor-pointer">Remember Me</div>
                 </div>
-                <div className="text-[#687588] font-medium text-s items-center cursor-default">Forgot Password</div>
+                <div className="text-[#687588] font-medium text-s items-center cursor-pointer">Forgot Password</div>
               </div>
               <Button className="w-full h-12" type="submit" variant="login">Login</Button>
             </form>
