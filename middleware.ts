296ce976cb7a8137f80c628f069bc8945ed606cb
Nathnael Desta <destanathnaelataro@gmail.com>
2025-03-18 14:09:42 +0300

    feat: add token time checking

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,29 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { jwtDecode } from 'jwt-decode';
+import { removeUserProfile } from './actions/auth';
 
 export async function middleware(request: NextRequest) {
-  let token = request.cookies.get('token');
-  let profile = request.cookies.get('profile');
-
-  console.log("token", token?.value || "No token found");
+  let token = request.cookies.get('token')?.value || null;
+  let profile = request.cookies.get('profile')?.value || null;
 
   const now = Math.floor(Date.now() / 1000);
+
+  // Decode token and check expiration
+  // Decode token and check expiration
+  if (token) {
+    try {
+      const decoded: { exp?: number } = jwtDecode(token);
+      console.log("compare the tow", decoded.exp, now)
+      if (decoded.exp && decoded.exp < now) {
+        console.log("Token expired");
+        removeUserProfile();
+      }
+    } catch (error) {
+      console.error("Invalid token:", error);
+    }
+  }
+
 
   if ((!token || !profile) && request.nextUrl.pathname != '/log-in') {
     const signInUrl = new URL('/log-in', request.url);
