411ecd5c3ea45c1cfa59b24816cc95ec18093fcb
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-03-25 22:14:27 +0300

    feat: add date formatting utility and implement order filtering by date in order actions

--- lib/utils.ts
+++ lib/utils.ts
@@ -17,3 +17,10 @@ export function generateTransactionId() {
   const randomNumbers = Math.floor(10000 + Math.random() * 90000);
   return `DTE${randomNumbers}`;
 }
+
+export function formatNewDate(date: Date): string {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
