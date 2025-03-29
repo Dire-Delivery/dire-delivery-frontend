eeef07a13f85f483503725be753ef54bc405fa09
EyosiSol <115923891+EyosiSol@users.noreply.github.com>
2025-03-29 13:20:53 +0300

    refactor: simplify layout component by removing unused font imports and updating body class

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/sonner';
 import { Toasterr } from '@/components/ui/toaster';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Dire Delivery',
@@ -29,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`font-general vsc-initialized`}>
         {children}
         <Toaster />
         <Toasterr />
